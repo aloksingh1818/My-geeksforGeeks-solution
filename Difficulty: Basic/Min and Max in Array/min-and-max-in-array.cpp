24a562e12dbd5705a5a814299dcbb81e5f506a6f
aloksingh1818 <138347516+aloksingh1818@users.noreply.github.com>
2024-10-17 15:02:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Min and Max in Array/min-and-max-in-array.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    pair<long long, long long> getMinMax(vector<long long int> arr) {
+        long long  ans = LLONG_MAX;
+        long long  ans2= LLONG_MIN;
+        
+        for(int i=0;i<arr.size();i++){
+           ans= min(arr[i],ans);
+           ans2=max(arr[i],ans2);
+           
+        }
+        return { ans,ans2};
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<long long int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        pair<long long, long long> pp = ob.getMinMax(arr);
+        cout << pp.first << " " << pp.second << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
